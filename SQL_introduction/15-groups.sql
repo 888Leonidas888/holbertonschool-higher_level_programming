134f114f8cf4daafb0dca71613cdebc42fbbc07f
Jhony <Jhonny14_1@hotmail.com>
2023-12-10 18:15:14 -0500

    Fix: Se actualiza script

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Este script agrupa los registros por puntaución y los cuenta.
-SELECT score, COUNT(score) as number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY number DESC;
