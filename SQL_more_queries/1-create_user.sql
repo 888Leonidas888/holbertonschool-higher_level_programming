93c7dcb1d1b2318f48e58ae349fa1f2cc291dce0
Jhony <Jhonny14_1@hotmail.com>
2023-12-12 17:29:33 -0500

    Feat: Este script crea un usuario con contraseña y le da todos los  permisos

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Este script crea un usuario, no debería fallar el código
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
