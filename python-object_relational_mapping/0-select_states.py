4b81089596da35e83a0553ed93256c2e141c285c
888Leonidas888 <jhonny14_1@hotmail.com>
2023-12-13 16:51:09 -0800

    Docs: Se modifica comentarios

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+"""
+Este módulo de prueba sirve para conectar
+con MySQL
+"""
+
+
 import MySQLdb
 import sys
 
 
 def main():
+    """
+    Esta función hace la consulta de la
+    tabla states en hbtn_0e_0_usa.
+    """
+
     db = MySQLdb.connect(
         host="localhost",
         user=sys.argv[1],
@@ -12,7 +23,7 @@ def main():
         database=sys.argv[3],
     )
     r = db.cursor()
-    r.execute("""SELECT * FROM states ORDER BY id ASC""")
+    r.execute("SELECT * FROM states ORDER BY id ASC")
     rows = r.fetchall()
 
     for row in rows:
@@ -23,4 +34,8 @@ def main():
 
 
 if __name__ == "__main__":
+    """
+    Esta validación evita que se ejecute
+    este archivo.
+    """
     main()
