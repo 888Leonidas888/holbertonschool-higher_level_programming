2d60c3c03ed508dedf2898739a97d1d3429bc46b
Jhony <Jhonny14_1@hotmail.com>
2023-12-17 21:03:35 -0500

    fix: se corrige sql

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- Crea un tabla unique_id
-IF TABLE NOT EXISTS CREATE unique_id (id INT DEFAULT 1, name VARCHAR(256), PRIMARY KEY (id))
+CREATE TABLE if NOT EXISTS unique_id (id INT DEFAULT 1, name VARCHAR(256), PRIMARY KEY (id))
