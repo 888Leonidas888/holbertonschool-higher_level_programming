8bf652424adce1e66d1733b8f048e0f06c5ccdf2
Jhony <Jhonny14_1@hotmail.com>
2023-12-17 21:19:06 -0500

    fix: se corrige sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
 -- crea un base de datos y un tabla
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY, name VARCHA(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY, name VARCHAR(256) NOT NULL);
