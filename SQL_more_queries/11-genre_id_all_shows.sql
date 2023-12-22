0e6490a6bbc0727de58363bf1fd4ccee78cf0ee2
Jhony <Jhonny14_1@hotmail.com>
2023-12-21 19:21:49 -0500

    docs: agrega comentario

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,18 +1,10 @@
+-- Lista tv_shows - tv_show_genres, si no encuentra genre_id asociado
+-- regresa NULL.
+
 --    SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL')
 --      FROM tv_shows 
 -- LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
 --  ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
-
--- SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
--- FROM tv_shows
--- LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
--- ORDER BY tv_shows.title ASC, genre_id ASC;
-
--- SELECT tv_shows.title, tv_show_genres.genre_id
--- FROM tv_shows
--- LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
--- WHERE tv_show_genres.genre_id IS NOT NULL
--- ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
 
    SELECT tv_shows.title, tv_show_genres.genre_id
      FROM tv_shows 
