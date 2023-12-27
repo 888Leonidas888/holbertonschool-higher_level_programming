0e2d7d0db188797d4878c75f8a12a8d5d3c61e19
Jhony <Jhonny14_1@hotmail.com>
2023-12-26 19:03:02 -0500

    chore: se modifica sql

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Lista el número de programas por genero
 
-  SELECT tv_genres.name AS genre, count(*) AS number_of_show
+  SELECT tv_genres.name AS genre, count(*) AS number_of_shows
     FROM tv_show_genres, tv_genres
    WHERE tv_show_genres.genre_id = tv_genres.id
 GROUP BY genre_id
-ORDER BY number_of_show DESC;
+ORDER BY number_of_shows DESC;
