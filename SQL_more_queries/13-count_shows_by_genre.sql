383e2cc4ddfc90a8e0d124eeb5fa88554de9efe4
Jhony <Jhonny14_1@hotmail.com>
2023-12-26 19:01:41 -0500

    feat: Lista el número de programas por genero

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Lista el número de programas por genero
+
+  SELECT tv_genres.name AS genre, count(*) AS number_of_show
+    FROM tv_show_genres, tv_genres
+   WHERE tv_show_genres.genre_id = tv_genres.id
+GROUP BY genre_id
+ORDER BY number_of_show DESC;
