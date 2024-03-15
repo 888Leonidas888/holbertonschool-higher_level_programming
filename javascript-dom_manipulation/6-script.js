3dfc17a527b74f2b38d6b40169044aff7ed5feff
Jhony <Jhonny14_1@hotmail.com>
2024-03-15 18:56:03 -0500

    feat: Consume una api con fecth()

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,31 @@
+const urlBase = "https://swapi-api.hbtn.io/api/people/1/?format=json";
+fetch(urlBase)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Error request failed!!!");
+    }
+
+    return response.json();
+  })
+  .then((data) => {
+    const character = data.name;
+    const characterElement = document.getElementById("character");
+    characterElement.innerText = character;
+  })
+  .catch((error) => {
+    console.error("Case: ", error);
+  });
+
+// Estructura de fetch
+// fetch(url)
+//     .then(response => {
+//         // Manipulación de la respuesta
+//     })
+//     .then(data => {
+//         // Manipulación de los datos
+//     })
+//     .catch(error => {
+//         // Manejo de errores
+//     });
+
+// fetch().then().then().catch()
